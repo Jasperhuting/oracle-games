033b0ff6fb3e6d65d82aa89635e2d1f95bb0e572
Jasper Huting <jasper.huting@gmail.com>
2025-12-07 20:23:49 +0100

    fixed padding

--- components/InboxComponent.tsx
+++ components/InboxComponent.tsx
@@ -150,7 +150,7 @@ export default function InboxComponent() {
   }
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 mt-[36px]">        
       <h1 className="text-3xl font-bold mb-6">Inbox</h1>
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
