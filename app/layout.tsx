ef038d50733e18ec4c8dbfc0346d70aee6b5c682
Jasper Huting <jasper.huting@gmail.com>
2025-12-16 18:29:33 +0100

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="nl">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Oracle games" />
+      </head>
       <body
         className={`${inter.variable} antialiased bg-gray-50`}
         style={{ fontFamily: 'var(--font-inter), Inter, system-ui, sans-serif' }}
