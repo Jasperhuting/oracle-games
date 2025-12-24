58ea6ba4b3a016768ddd8eb4d90193611a1f8319
Jasper Huting <jasper.huting@gmail.com>
2025-12-24 16:02:14 +0100

    FRA -> fr

--- components/AddRiderTab.tsx
+++ components/AddRiderTab.tsx
@@ -267,7 +267,7 @@ export function AddRiderTab() {
               value={formData.country}
               onChange={(e) => setFormData({ ...formData, country: e.target.value })}
               required
-              placeholder="FRA"
+              placeholder="fr"
               className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
